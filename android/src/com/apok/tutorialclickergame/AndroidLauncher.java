ec6c024faa348f8ff05e9df67d73dc919916c1a9
Apokrbk <damian.portasinski@gmail.com>
2017-02-27 16:52:03 +0100

    init

---
+++ android/src/com/apok/tutorialclickergame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.apok.tutorialclickergame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.apok.tutorialclickergame.TutorialClickerGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TutorialClickerGame(), config);
+	}
+}
