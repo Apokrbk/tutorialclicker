66129db12811b4e7d20261fddb135157e5e95e91
Apokrbk <damian.portasinski@gmail.com>
2017-02-27 18:54:02 +0100

    Added change to gameplayscreen

--- core/src/com/apok/tutorialclickergame/screens/SplashScreen.java
+++ core/src/com/apok/tutorialclickergame/screens/SplashScreen.java
@@ -2,6 +2,7 @@ package com.apok.tutorialclickergame.screens;
 
 import com.apok.tutorialclickergame.TutorialClickerGame;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Timer;
 
 /**
  * Created by Apok on 27.02.2017.
@@ -12,9 +13,15 @@ public class SplashScreen extends AbstractScreen{
     private Texture splashImg;
 
 
-    public SplashScreen(TutorialClickerGame game) {
+    public SplashScreen(final TutorialClickerGame game) {
         super(game);
         init();
+        Timer.schedule(new Timer.Task() {
+            @Override
+            public void run() {
+                game.setScreen(new GameplayScreen(game));
+            }
+        }, 1);
     }
 
     private void init() {
