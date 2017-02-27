d885ae0c20fdaa8467725e1abd040155056ee8b7
Apokrbk <damian.portasinski@gmail.com>
2017-02-27 18:42:26 +0100

    Added dummy gameplay screen

---
+++ core/src/com/apok/tutorialclickergame/screens/GameplayScreen.java
@@ -0,0 +1,18 @@
+package com.apok.tutorialclickergame.screens;
+
+import com.apok.tutorialclickergame.TutorialClickerGame;
+
+/**
+ * Created by Apok on 27.02.2017.
+ */
+
+public class GameplayScreen extends AbstractScreen {
+    public GameplayScreen(TutorialClickerGame game) {
+        super(game);
+        init();
+    }
+
+    private void init() {
+    }
+    
+}
