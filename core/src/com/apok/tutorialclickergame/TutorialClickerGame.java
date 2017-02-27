edb00ceba9838505bf16c3aaeb73be2f7ad058fa
Apokrbk <damian.portasinski@gmail.com>
2017-02-27 17:33:10 +0100

    config desktop launcher

--- core/src/com/apok/tutorialclickergame/TutorialClickerGame.java
+++ core/src/com/apok/tutorialclickergame/TutorialClickerGame.java
@@ -1,33 +1,34 @@
 package com.apok.tutorialclickergame;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.apok.tutorialclickergame.screens.SplashScreen;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class TutorialClickerGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class TutorialClickerGame extends Game {
+
+	public static final int WIDTH = 480;
+	public static final int HEIGHT = 800;
+	public static final String TITLE = "Tutorial Clicker";
+
+	private boolean paused;
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void create () {
+		this.setScreen(new SplashScreen(this));
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
+	/*******
+	GETTERS AND SETTERS
+	*******/
+
+	public boolean isPaused() {
+		return paused;
+	}
+
+	public void setPaused(boolean paused) {
+		this.paused = paused;
 	}
 }
