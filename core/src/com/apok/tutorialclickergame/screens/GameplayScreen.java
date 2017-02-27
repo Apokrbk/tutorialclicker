bf20fc46e8ff6bc388558b9d5aa8ffbe3a65bc72
Apokrbk <damian.portasinski@gmail.com>
2017-02-27 19:06:41 +0100

    Displaying player image

--- core/src/com/apok/tutorialclickergame/screens/GameplayScreen.java
+++ core/src/com/apok/tutorialclickergame/screens/GameplayScreen.java
@@ -1,18 +1,40 @@
 package com.apok.tutorialclickergame.screens;
 
 import com.apok.tutorialclickergame.TutorialClickerGame;
+import com.apok.tutorialclickergame.entities.Player;
 
 /**
  * Created by Apok on 27.02.2017.
  */
 
 public class GameplayScreen extends AbstractScreen {
+
+    private Player player;
+
     public GameplayScreen(TutorialClickerGame game) {
         super(game);
         init();
     }
 
     private void init() {
+        initPlayer();
     }
-    
+
+    private void initPlayer() {
+        player = new Player();
+        stage.addActor(player);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update();
+        spriteBatch.begin();
+        stage.draw();
+        spriteBatch.end();
+    }
+
+    private void update() {
+        stage.act();
+    }
 }
