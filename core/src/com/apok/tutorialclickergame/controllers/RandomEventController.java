9c49003ae356f3cab9b0f278fad8e7ef1a00c9d1
Apokrbk <damian.portasinski@gmail.com>
2017-03-02 18:47:59 +0100

    added random event controller

---
+++ core/src/com/apok/tutorialclickergame/controllers/RandomEventController.java
@@ -0,0 +1,13 @@
+package com.apok.tutorialclickergame.controllers;
+
+/**
+ * Created by Apok on 02.03.2017.
+ */
+
+public class RandomEventController {
+
+    public RandomEventController()
+    {
+
+    }
+}
